10363d3b5a3ca77f71db0b90db1b2e4c6a54cc20
Christian Sanches <christian.sanches-v@uplevelteam.com>
2026-03-16 18:36:55 -0300

    creating the expense-tracker structure

---
+++ config/settings/local.py
@@ -0,0 +1,12 @@
+# config/settings/local.py
+from .base import *
+
+DEBUG = True
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+INTERNAL_IPS = ["127.0.0.1"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
